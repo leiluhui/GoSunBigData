d3062a232d927eefb1b87a8e5e9cd2ef4e3e5f93
ck0903 <ck@hzgosun.com>
2018-08-21 18:56:46 +0800

    人口管理模块

--- Service/PeopleManager/src/main/java/com/hzgc/service/clustering/service/ParseByOption.java
+++ Service/PeopleManager/src/main/java/com/hzgc/service/clustering/service/ParseByOption.java
@@ -381,7 +381,7 @@ public class ParseByOption {
                     whereParamList.add(regionList.get(0));
                 } else {
                     whereQuery.append(PeopleManagerTable.REGION)
-                            .append(" like '%").append(regionList.get(0)).append("%'");
+                            .append(" = '").append(regionList.get(0)).append("'");
                     whereParamList.add(regionList.get(0));
                     isChanged = true;
                 }
