7fa0522201e990a2fb8d1b2861f694ec31569561
lsy521 <lemon52121@163.com>
2018-09-17 17:51:17 +0800

    小区人口数量统计,实有,重点,关爱查询

--- Service/People/src/main/java/com/hzgc/service/people/controller/CommunityController.java
+++ Service/People/src/main/java/com/hzgc/service/people/controller/CommunityController.java
@@ -1,18 +1,25 @@
 package com.hzgc.service.people.controller;
 
+import com.hzgc.common.service.error.RestErrorCode;
 import com.hzgc.common.service.response.ResponseResult;
 import com.hzgc.common.service.rest.BigDataPath;
+import com.hzgc.common.util.json.JacksonUtil;
 import com.hzgc.service.people.param.CommunityPeopleCountVO;
+import com.hzgc.service.people.param.CommunityPeopleDTO;
 import com.hzgc.service.people.param.CommunityPeopleVO;
 import com.hzgc.service.people.service.CommunityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@Slf4j
 @Api(value = "/community", tags = "社区人口库服务")
 public class CommunityController {
     @Autowired
@@ -21,25 +28,76 @@ public class CommunityController {
     @ApiOperation(value = "小区人口数量统计", response = CommunityPeopleCountVO.class)
     @RequestMapping(value = BigDataPath.COMMUNITY_COUNT, method = RequestMethod.GET)
     public ResponseResult<CommunityPeopleCountVO> countCommunityPeople(Long communityId) {
-        return null;
+        if (communityId == null){
+            log.error("Start count community people, but community id is null !");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"小区ID不能为空，请检查！");
+        }
+        log.info("Start count community people, param is: " + communityId);
+        CommunityPeopleCountVO communityPeopleCountVO = communityService.countCommunityPeople(communityId);
+        return ResponseResult.init(communityPeopleCountVO);
     }
 
     @ApiOperation(value = "小区实有人口查询", response = CommunityPeopleVO.class)
     @RequestMapping(value = BigDataPath.COMMUNITY_PEOPLE, method = RequestMethod.GET)
-    public ResponseResult<CommunityPeopleVO> searchCommunityPeople(Long communityId) {
-        return null;
+    public ResponseResult<List<CommunityPeopleVO>> searchCommunityPeople(CommunityPeopleDTO dto) {
+        if (dto == null){
+            log.error("Start search community people, but param is null !");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"查询参数为空，请检查！");
+        }
+        if (dto.getCommunityId() == null){
+            log.error("Start search community people, but communityId is null !");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"小区ID不能为空，请检查！");
+        }
+        if (dto.getLimit() == 0){
+            log.error("Start search community people, but limit is 0 !");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"分页行数不能为0，请检查！");
+        }
+        log.info("Start search community people, param is: "+ JacksonUtil.toJson(dto));
+        List<CommunityPeopleVO> communityPeopleVO = communityService.searchCommunityPeople(dto);
+        log.info("Search community people successfully!");
+        return ResponseResult.init(communityPeopleVO);
     }
 
     @ApiOperation(value = "小区重点人口查询", response = CommunityPeopleVO.class)
     @RequestMapping(value = BigDataPath.COMMUNITY_PEOPLE_STATUS, method = RequestMethod.GET)
-    public ResponseResult<CommunityPeopleVO> searchCommunityStatusPeople(Long communityId) {
-        return null;
+    public ResponseResult<List<CommunityPeopleVO>> searchCommunityImportantPeople(CommunityPeopleDTO param) {
+        if (param == null){
+            log.error("Start search people ,but param is null ");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"查询参数为空,请检查！");
+        }
+        if (param.getCommunityId() == null || param.getCommunityId() == 0){
+            log.error("Start search people ,but community id is null ");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"小区ID不能为空,请检查！");
+        }
+        if (param.getLimit() == 0){
+            log.error("Start search people ,but limit is 0");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"分页行数不能为0,请检查！");
+        }
+        log.info("Start search important people,param is :"+ JacksonUtil.toJson(param));
+        List<CommunityPeopleVO> voList = communityService.searchCommunityImportantPeople(param);
+        log.info("Search community important people successfully!");
+        return ResponseResult.init(voList);
     }
 
     @ApiOperation(value = "小区关爱人口查询", response = CommunityPeopleVO.class)
     @RequestMapping(value = BigDataPath.COMMUNITY_PEOPLE_CARE, method = RequestMethod.GET)
-    public ResponseResult<CommunityPeopleVO> searchCommunityCarePeople(Long communityId) {
-        return null;
+    public ResponseResult<List<CommunityPeopleVO>> searchCommunityCarePeople(CommunityPeopleDTO param) {
+        if (param == null){
+            log.error("Start search care people ,but param is null ");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"查询参数为空,请检查！");
+        }
+        if (param.getCommunityId() == null || param.getCommunityId() == 0){
+            log.error("Start search care people ,but community id is null ");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"小区ID不能为空,请检查！");
+        }
+        if (param.getLimit() == 0){
+            log.error("Start search care people ,but limit is 0 ");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT,"分页行数不能为0,请检查！");
+        }
+        log.info("Start search care people,param is :"+ JacksonUtil.toJson(param));
+        List<CommunityPeopleVO> voList = communityService.searchCommunityCarePeople(param);
+        log.info("Search community care people successfully!");
+        return ResponseResult.init(voList);
     }
 
     @ApiOperation(value = "小区新增人口查询", response = ResponseResult.class)
--- Service/People/src/main/java/com/hzgc/service/people/service/CommunityService.java
+++ Service/People/src/main/java/com/hzgc/service/people/service/CommunityService.java
@@ -1,10 +1,19 @@
 package com.hzgc.service.people.service;
 
+import com.github.pagehelper.PageHelper;
 import com.hzgc.service.people.dao.NewPeopleMapper;
 import com.hzgc.service.people.dao.PeopleMapper;
+import com.hzgc.service.people.model.People;
+import com.hzgc.service.people.param.CommunityPeopleCountVO;
+import com.hzgc.service.people.param.CommunityPeopleDTO;
+import com.hzgc.service.people.param.CommunityPeopleVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,4 +22,57 @@ public class CommunityService {
     private PeopleMapper peopleMapper;
     @Autowired
     private NewPeopleMapper newPeopleMapper;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public CommunityPeopleCountVO countCommunityPeople(Long communityId) {
+        CommunityPeopleCountVO vo = new CommunityPeopleCountVO();
+        int communityPeople = peopleMapper.countCommunityPeople(communityId);
+        int importantPeople = peopleMapper.countImportantPeople(communityId);
+        int carePeople = peopleMapper.countCarePeople(communityId);
+        int newPeople = 0;
+        int outPeople = 0;
+        vo.setCommunityPeoples(communityPeople);
+        vo.setImportantPeoples(importantPeople);
+        vo.setCarePeoples(carePeople);
+        vo.setNewPeoples(newPeople);
+        vo.setOutPeoples(outPeople);
+        return vo;
+    }
+
+    public  List<CommunityPeopleVO> searchCommunityPeople(CommunityPeopleDTO param) {
+        PageHelper.offsetPage(param.getStart(), param.getLimit());
+        List<People> peopleList = peopleMapper.searchCommunityPeople(param.getCommunityId());
+        return this.shift(peopleList);
+    }
+
+    public List<CommunityPeopleVO> searchCommunityImportantPeople(CommunityPeopleDTO param) {
+        List<CommunityPeopleVO> voList = new ArrayList();
+        PageHelper.offsetPage(param.getStart(), param.getLimit());
+        List<People> peopleList = peopleMapper.searchImportantPeople(param.getCommunityId());
+        return this.shift(peopleList);
+    }
+
+    public List<CommunityPeopleVO> searchCommunityCarePeople(CommunityPeopleDTO param) {
+        PageHelper.offsetPage(param.getStart(), param.getLimit());
+        List<People> peopleList = peopleMapper.searchCarePeople(param.getCommunityId());
+        return this.shift(peopleList);
+    }
+
+    private List<CommunityPeopleVO> shift( List<People> peopleList){
+        List<CommunityPeopleVO> voList = new ArrayList<>();
+        if (peopleList != null && peopleList.size() > 0) {
+            for (People people : peopleList) {
+                CommunityPeopleVO vo = new CommunityPeopleVO();
+                vo.setId(people.getId());
+                vo.setIdCard(people.getIdcard());
+                vo.setName(people.getName());
+                if (people.getLasttime() != null) {
+                    vo.setLastTime(sdf.format(people.getLasttime()));
+                }
+                voList.add(vo);
+            }
+        }
+        return voList;
+    }
 }
