9b5cd960f3997ffa185e47b00c51986b9ee7f5ee
ck0903 <ck@hzgosun.com>
2018-09-19 09:57:43 +0800

    添加客户端逻辑以及数据融合模块

---
+++ Cluster/PeopleManager-background/PeopleManager-background-client/src/main/java/com/hzgc/cluster/peoman/client/service/StartJobClient.java
@@ -0,0 +1,28 @@
+package com.hzgc.cluster.peoman.client.service;
+
+import com.hzgc.cluster.peoman.client.zookeeper.Constant;
+import com.hzgc.cluster.peoman.client.zookeeper.JobDiscover;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Slf4j
+public class StartJobClient {
+    @Autowired
+    private JobDiscover jobDiscover;
+    @Autowired
+    private JobClientCallBack jobClientCallBack;
+
+    //开始计算worker的个数并从TIDB中读取t_picture的个数，计算出每个节点需要启动多少个数据
+    @PostConstruct
+    public void catchNodeNumAndStartJob(){
+        try {
+            jobDiscover.startListen(jobClientCallBack, Constant.rootPath);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
