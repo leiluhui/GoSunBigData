8b8349c6eeb55db240d17a6199ce65e7a19d2da3
lsy521 <lemon52121@163.com>
2018-09-27 18:15:19 +0800

    update

---
+++ Service/People/src/main/java/com/hzgc/service/community/param/CapturePictureInfo.java
@@ -0,0 +1,18 @@
+package com.hzgc.service.community.param;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@ApiModel(value ="迁入人员抓拍统计（含总条数）")
+@Data
+public class CapturePictureInfo implements Serializable {
+    @ApiModelProperty(value = "抓拍设备")
+    private String deviceId;
+    @ApiModelProperty(value = "抓拍时间")
+    private String captureTime;
+    @ApiModelProperty(value = "抓拍照片")
+    private String picture;     // 暂定String
+}
