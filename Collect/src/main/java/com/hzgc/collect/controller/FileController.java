b32795685b742b6583fe2ec5ea6ae5f63b54c8df
zhaozhe <dgxdks@163.com>
2018-10-09 09:53:04 +0800

    modify

--- Collect/src/main/java/com/hzgc/collect/controller/FileController.java
+++ Collect/src/main/java/com/hzgc/collect/controller/FileController.java
@@ -4,6 +4,9 @@ import com.hzgc.collect.service.http.HttpFile;
 import com.hzgc.common.service.api.service.InnerService;
 import com.hzgc.common.util.json.JacksonUtil;
 import com.hzgc.jniface.PictureData;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "文件服务")
 @RestController
 @Slf4j
 public class FileController {
@@ -21,6 +25,7 @@ public class FileController {
     private HttpFile httpFile;
 
     @RequestMapping("/image")
+    @ApiOperation(value = "获取原图片", produces = "image/jpeg")
     public ResponseEntity<byte[]> getImage(@RequestParam(name = "url") String path) {
         log.info("Receive file path:" + path);
         byte[] image;
@@ -36,6 +41,7 @@ public class FileController {
     }
 
     @RequestMapping("/image_zip")
+    @ApiOperation(value = "获取压缩图片", produces = "image/jpeg")
     public ResponseEntity<byte[]> getZipImage(@RequestParam(name = "url") String path) {
         log.info("Receive file path:" + path + ", zip image");
         byte[] image;
