644c3854168b5ea835ff58fe332b40db62f28d16
ck0903 <ck@hzgosun.com>
2018-10-09 10:20:39 +0800

    修改bug

--- Service/People/src/main/java/com/hzgc/service/PeopleApplication.java
+++ Service/People/src/main/java/com/hzgc/service/PeopleApplication.java
@@ -6,9 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
