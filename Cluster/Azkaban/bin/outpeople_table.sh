6ae6d63a4e77bf10cfeb31baaadf427b35e6f4a8
zbl <1018732456@qq.com>
2018-10-10 15:57:30 +0800

    modify

--- Cluster/Azkaban/bin/outpeople_table.sh
+++ Cluster/Azkaban/bin/outpeople_table.sh
@@ -5,17 +5,18 @@ PORT=4000
 use people;
 
 INSERT INTO t_people_out (peopleid, community, \`month\`, isconfirm)
-SELECT t1.id,t1.community ,time,1
+SELECT t1.id, t1.community ,time, 1
 FROM(
     SELECT community ,time, people.id
     FROM (
         SELECT id, community, DATE_FORMAT(lasttime,"%Y%m") AS time
         FROM t_people
-        WHERE lasttime <= DATE_SUB(now(),INTERVAL 3 MONTH)
+        WHERE lasttime <= DATE_SUB(now(),INTERVAL 1 DAY)
           AND community is NOT NULL
-    ) AS people, t_picture
-    WHERE people.id = t_picture.peopleid
-)AS t1 ,t_people_out
-WHERE t1.id !=t_people_out.peopleid
+    ) AS people LEFT JOIN t_picture
+    ON people.id = t_picture.peopleid
+)AS t1 LEFT JOIN t_people_out
+ON t1.id != t_people_out.peopleid
 AND time = t_people_out.\`month\`;
+
 EOF
