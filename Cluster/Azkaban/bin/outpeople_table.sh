f547a8673c4181b2c982ec600d913141743250ef
zbl <1018732456@qq.com>
2018-10-18 19:54:20 +0800

    modify

--- Cluster/Azkaban/bin/outpeople_table.sh
+++ Cluster/Azkaban/bin/outpeople_table.sh
@@ -8,7 +8,7 @@ INSERT INTO t_people_out (peopleid, community, `month`, isconfirm)
 SELECT id,community ,DATE_FORMAT(NOW(),"%Y%m"),1  FROM (
 SELECT DISTINCT community ,time, people.id
 FROM (
-		SELECT id, community, DATE_FORMAT(lasttime,"%Y%m") AS time
+        SELECT id, community, DATE_FORMAT(lasttime,"%Y%m") AS time
         FROM t_people
         WHERE community is NOT NULL
           AND (lasttime <= DATE_SUB(now(),INTERVAL 3 MONTH)
