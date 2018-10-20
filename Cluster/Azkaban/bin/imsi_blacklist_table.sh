c78f4a14f3f6ae271ec5fd2151f9368c3b6e60fb
zbl <1018732456@qq.com>
2018-10-20 08:53:26 +0800

    modify

--- Cluster/Azkaban/bin/imsi_blacklist_table.sh
+++ Cluster/Azkaban/bin/imsi_blacklist_table.sh
@@ -5,13 +5,14 @@ PORT=4000
 use people;
 
 INSERT INTO t_imsi_blacklist (imsi,currenttime)
-SELECT f1.imsi, f2.currenttime
-FROM (SELECT imsi, savetime
+SELECT DISTINCT f1.imsi, f2.currenttime
+FROM (SELECT imsi, savetime, COUNT(imsi) AS count
       FROM (
-            SELECT imsi, savetime ,COUNT(imsi) AS count
+            SELECT imsi, savetime
             FROM t_imsi_all
-            WHERE FROM_UNIXTIME(savetime/1000,"%Y%m%d")=DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),"%Y%m%d")) AS t1
-      WHERE count >=10)AS f1 ,t_imsi_filter AS f2
+            WHERE FROM_UNIXTIME(savetime/1000,"%Y%m%d")=DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 0 DAY),"%Y%m%d")) AS t1
+      GROUP BY imsi,FROM_UNIXTIME(savetime/1000,"%Y%m%d")
+      HAVING COUNT(imsi)>=10)AS f1 ,t_imsi_filter AS f2
 WHERE f1.imsi = f2.imsi
 AND FROM_UNIXTIME(f1.savetime/1000,"%Y%m%d")=f2.currenttime;
 EOF
