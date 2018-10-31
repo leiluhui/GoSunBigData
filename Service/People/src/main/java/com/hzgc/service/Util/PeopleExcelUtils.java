18ee6ff744ad1745f56803025b22ca741aed1b15
hangde1314 <273967108@qq.com>
2018-10-31 15:00:29 +0800

    excel update

--- Service/People/src/main/java/com/hzgc/service/Util/PeopleExcelUtils.java
+++ Service/People/src/main/java/com/hzgc/service/Util/PeopleExcelUtils.java
@@ -1,4 +1,4 @@
-package com.hzgc.service.dispatch.Util;
+package com.hzgc.service.Util;
 
 import com.hzgc.common.service.api.service.PlatformService;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -22,13 +22,13 @@ import java.util.regex.Pattern;
  *
  * @author yinhang
  */
-public class DispatchExcelUtils {
-    private Logger logger = LoggerFactory.getLogger(DispatchExcelUtils.class);
+public class PeopleExcelUtils {
+    private Logger logger = LoggerFactory.getLogger(PeopleExcelUtils.class);
     private Workbook wb;
     private Sheet sheet;
     private Row row;
 
-    public DispatchExcelUtils(MultipartFile file) {
+    public PeopleExcelUtils(MultipartFile file) {
         if (file == null) {
             return;
         }
