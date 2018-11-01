1be4165f79e4306a1e692487d834ea67945486f1
zhaozhe <dgxdks@163.com>
2018-11-01 20:04:55 +0800

    add Dockerfile for collect

---
+++ Service/Collect/docker/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+exec java -jar app.jar --spring.profiles.active=pro --spring.cloud.config.enabled=false --eureka.ip=${EUREKA_IP} --zookeeper.host=${ZOOKEEPER_HOST} --eureka.port=${EUREKA_PORT}
