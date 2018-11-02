0a808b7567ca7a71c0f2ebc3d03730c119debfa4
test <121921013@qq.com>
2018-11-02 11:49:21 +0800

    'update'

---
+++ Service/Imsi-Dynrepo/docker/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+exec java -jar app.jar --spring.profiles.active=pro --spring.cloud.config.enabled=false --eureka.ip=${EUREKA_IP} --zookeeper.host=${ZOOKEEPER_HOST} --eureka.port=${EUREKA_PORT} --mysql.host=${MYSQL_HOST} --query.time=${QUERY_TIME} --kafka.host=${KAFKA_HOST}
