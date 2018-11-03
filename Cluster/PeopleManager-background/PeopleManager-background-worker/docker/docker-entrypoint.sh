79c560d5a1668c10bd1e9c21b54537cb9696754a
zhaozhe <dgxdks@163.com>
2018-11-03 11:32:28 +0800

    modify

---
+++ Cluster/PeopleManager-background/PeopleManager-background-worker/docker/docker-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#/bin/bash
+
+exec java -jar app.jar --spring.profiles.active=pro --spring.cloud.config.enabled=false --kafka.host=${KAFKA_HOST} --zookeeper.address=${ZK_ADDRESS} --compare.number=${COMPARE_NUMBER} --bit.threshold=${BIT_THRESHOLD} --float.threshold=${FLOAT_THRESHOLD} --float.new.threshold=${FLOAT_NEW_THRESHOLD} --float.compare.open=${FLOAT_COMPARE_OPEN} --mysql.host=${MYSQL_HOST} --kafka.host=${KAFKA_HOST}
