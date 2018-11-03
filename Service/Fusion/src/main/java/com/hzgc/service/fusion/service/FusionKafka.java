ae04ab961981c46d14ea152dfa21e44e847b4b33
ck0903 <ck@hzgosun.com>
2018-11-03 14:16:11 +0800

    'dependency'

--- Service/Fusion/src/main/java/com/hzgc/service/fusion/service/FusionKafka.java
+++ Service/Fusion/src/main/java/com/hzgc/service/fusion/service/FusionKafka.java
@@ -10,7 +10,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
