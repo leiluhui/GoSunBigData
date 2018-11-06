1c4d0e779942616ef510658a19e691e97b5b070f
test <121921013@qq.com>
2018-11-06 15:42:48 +0800

    'update'

--- Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/ImsiApplication.java
+++ Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/ImsiApplication.java
@@ -1,13 +1,9 @@
 package com.hzgc.service.imsi;
 
-import com.hzgc.common.service.api.config.EnableInnerService;
-import com.hzgc.service.imsi.service.ImsiProducer;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan(value = "com.hzgc.service.imsi.dao")
 @EnableTransactionManagement
 @EnableKafka
-@EnableInnerService
 public class ImsiApplication {
 
     public static void main(String[] args) {
--- Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/controller/ImsiController.java
+++ Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/controller/ImsiController.java
@@ -1,18 +1,10 @@
 package com.hzgc.service.imsi.controller;
 
-import com.hzgc.common.service.api.service.InnerService;
-import com.hzgc.common.service.error.RestErrorCode;
-import com.hzgc.common.service.response.ResponseResult;
 import com.hzgc.common.service.rest.BigDataPath;
-import com.hzgc.common.util.json.JacksonUtil;
-import com.hzgc.jniface.PictureData;
-import com.hzgc.seemmo.util.BASE64Util;
 import com.hzgc.service.imsi.model.ImsiInfo;
-import com.hzgc.service.imsi.model.MacInfo;
 import com.hzgc.service.imsi.service.ImsiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +18,6 @@ public class ImsiController {
     @Autowired
     private ImsiService imsiService;
 
-    @Autowired
-    private InnerService innerService;
-
     @RequestMapping(value = BigDataPath.IMSI_SEARCH_BY_TIME, method = RequestMethod.GET)
     public List <ImsiInfo> queryByTime(Long time) {
         if (null == time) {
@@ -37,12 +26,5 @@ public class ImsiController {
         }
         log.info("Start search imsi by time, this time is: " + time);
         return imsiService.queryByTime(time);
-    }
-
-    @RequestMapping(value = "/aaa", method = RequestMethod.GET)
-    public void sjdajd(){
-        String imageStr = BASE64Util.getImageStr("C:\\Users\\g10255\\Desktop\\测试图片数据\\08.31.23[M][0@0][face1].jpg");
-        PictureData pictureData = innerService.faceFeautreExtract(imageStr);
-        System.out.println(JacksonUtil.toJson(pictureData));
     }
 }
