a5a6c3426c358ef8752174af9d7ffdcadd94922a
Zz897426182 <dgxdks@163.com>
2018-11-07 02:00:50 +0800

    complete service module

--- Service/People/docker/docker-entrypoint.sh
+++ Service/People/docker/docker-entrypoint.sh
@@ -1,3 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-exec java -jar app.jar --spring.profiles.active=pro --spring.cloud.config.enabled=false --eureka.ip=${EUREKA_IP} --zookeeper.host=${ZOOKEEPER_HOST} --eureka.port=${EUREKA_PORT} --mysql.host=${MYSQL_HOST} --mysql.username=${MYSQL_USERNAME}
+exec java -jar app.jar \
+--spring.profiles.active=pro \
+--spring.cloud.config.enabled=false \
+--eureka.ip=${EUREKA_IP} \
+--eureka.port=${EUREKA_PORT} \
+--mysql.host=${MYSQL_HOST} \
+--mysql.username=${MYSQL_USERNAME} \
+--mysql.password=${MYSQL_PASSOWRD}
