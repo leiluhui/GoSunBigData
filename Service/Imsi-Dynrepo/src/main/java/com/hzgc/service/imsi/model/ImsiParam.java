a3fd66f32d02d55914c514bc34a405a23078b279
SenYan-java <121921013@qq.com>
2018-11-17 15:11:11 +0800

    update

--- Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/model/ImsiParam.java
+++ Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/model/ImsiParam.java
@@ -3,8 +3,6 @@ package com.hzgc.service.imsi.model;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Data
 public class ImsiParam implements Serializable {
@@ -12,11 +10,4 @@ public class ImsiParam implements Serializable {
     private String startTime;
     //结束时间
     private String endTime;
-
-    public static void main(String[] args) throws Exception{
-        String time = "2018-11-17 15:00:00";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date parse = sdf.parse(time);
-        System.out.printf("" + parse.getTime());
-    }
 }
