8b49779885035f17dc9252e830a4ed097163b94a
cdb156 <15658067087@163.com>
2018-11-20 10:11:31 +0800

    修改spark配置

--- Cluster/Spark/docker/start-kafka-to-tidb.sh
+++ Cluster/Spark/docker/start-kafka-to-tidb.sh
@@ -2,5 +2,8 @@
 
 spark-submit \
 --master local[*] \
+--conf "spark.driver.extraJavaOptions=-Dkafka_broker=${KAFKA_BROKER} -Dzk_address=${ZK_ADDRESS} -Djdbc_ip=${JDBC_IP}
+-Ddriver=com.mysql.jdbc.Driver -Dapp_name=KafkaToTidb -Dcheck_point=/tmp/spark_out -Dgroup_id=1 -Dtime_out=10000 -Dtopic=imsi
+-Djdbc_port=${JDBC_PORT}" \
 --class com.hzgc.cluster.spark.consumer.KafkaToTidb \
 /spark.jar
