147e5c67fccffe2ba1a955b03a98d9dc519c546e
hangde1314 <273967108@qq.com>
2018-11-20 15:59:46 +0800

    poeple import and people重点人员识别记录

--- Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/controller/MacController.java
+++ Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/controller/MacController.java
@@ -4,9 +4,9 @@ import com.hzgc.common.service.error.RestErrorCode;
 import com.hzgc.common.service.response.ResponseResult;
 import com.hzgc.common.service.rest.BigDataPath;
 import com.hzgc.common.util.json.JacksonUtil;
-import com.hzgc.service.imsi.model.MacInfo;
-import com.hzgc.service.imsi.model.MacParam;
+import com.hzgc.service.imsi.model.*;
 import com.hzgc.service.imsi.service.MacService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,5 +31,30 @@ public class MacController {
         }
         log.info("Start search mac by sns, this sns is: " + JacksonUtil.toJson(macParam));
         return macService.queryBySns(macParam);
+    }
+
+    @ApiOperation(value = "MAC信息模糊查询", response = MacVO.class)
+    @RequestMapping(value = BigDataPath.SEARCH_MAC, method = RequestMethod.POST)
+    public ResponseResult<List<MacVO>> searchIMSI(@RequestBody SearchMacDTO searchMacDTO) {
+        if (searchMacDTO == null) {
+            log.error("Start search mac info, but searchMacDTO is null");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT, "查询参数为空,请检查！");
+        }
+        if (searchMacDTO.getSearchType() != 0 && searchMacDTO.getSearchType() != 1 && searchMacDTO.getSearchType() != 2) {
+            log.error("Start search mac info, but searchType is error");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT, "查询类型错误，请检查");
+        }
+        if (searchMacDTO.getStart() < 0) {
+            log.error("Start search mac info, but start < 0");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT, "起始行数不能小于0,请检查！");
+        }
+        if (searchMacDTO.getLimit() <= 0) {
+            log.error("Start search mac info, but limit <= 0");
+            return ResponseResult.error(RestErrorCode.ILLEGAL_ARGUMENT, "分页行数不能小于或等于0,请检查！");
+        }
+        log.info("Start search mac info, searchMacDTO is:" + JacksonUtil.toJson(searchMacDTO));
+        ResponseResult<List<MacVO>> macVOList = macService.searchIMSI(searchMacDTO);
+        log.info("Start search mac info successfully");
+        return macVOList;
     }
 }
---
+++ Service/Imsi-Dynrepo/src/main/java/com/hzgc/service/imsi/model/SearchMacDTO.java
@@ -0,0 +1,27 @@
+package com.hzgc.service.imsi.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@ApiModel(value = "MAC信息入参封装")
+@Data
+public class SearchMacDTO implements Serializable {
+    @ApiModelProperty(value = "查询类型（0：ID，1：设备，2：mac）")
+    @NotNull
+    private int searchType;
+    @ApiModelProperty(value = "查询内容")
+    @NotNull
+    private String searchVal;
+    @ApiModelProperty(value = "小区ID")
+    private String communityId;
+    @ApiModelProperty(value = "起始行数")
+    @NotNull
+    private int start;
+    @ApiModelProperty(value = "分页行数")
+    @NotNull
+    private int limit;
+}
