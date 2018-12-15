3d232d9de6570b7479a3c5638a68cfd9b92ef182
cdb156 <15658067087@163.com>
2018-12-15 14:37:50 +0800

    facecompare，dispatch-background 版本独立

--- Analysis-Platform/Dispatch-background/src/main/java/com/hzgc/cluster/dispatch/DispatchApplication.java
+++ Analysis-Platform/Dispatch-background/src/main/java/com/hzgc/cluster/dispatch/DispatchApplication.java
@@ -6,13 +6,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnablePlatformService
 @EnableAutoConfiguration
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableInnerService
 @MapperScan("com.hzgc.cluster.dispatch.dao")
 public class DispatchApplication {
