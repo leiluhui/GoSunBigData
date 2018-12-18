ce0838ccea949d7d33b2d83382494fbe13efe383
lsy521 <lemon52121@163.com>
2018-12-19 03:22:57 +0800

    imsi bug

--- Cloud-Platform/Imsi-Dynrepo/src/main/java/com/hzgc/cloud/imsi/service/ImsiService.java
+++ Cloud-Platform/Imsi-Dynrepo/src/main/java/com/hzgc/cloud/imsi/service/ImsiService.java
@@ -50,7 +50,7 @@ public class ImsiService {
                 List<Long> communityIds = platformService.getCommunityIdsById(Long.valueOf(searchImsiDTO.getLac()));
                 if (communityIds == null || communityIds.size() == 0){
                     log.info("Search community ids by region id is null, so return null, region id:" + searchImsiDTO.getLac());
-                    return null;
+                    return ResponseResult.error(3,"Search community ids by region id is null");
                 }
                 param.setCommunityIds(communityIds);
             }
