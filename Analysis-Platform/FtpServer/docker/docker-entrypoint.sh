3a63b53d989078ab9795fe2369c7dd94c7d64824
hangde1314 <273967108@qq.com>
2018-12-24 16:37:04 +0800

    update

--- Analysis-Platform/FtpServer/docker/docker-entrypoint.sh
+++ Analysis-Platform/FtpServer/docker/docker-entrypoint.sh
@@ -7,7 +7,7 @@ elif [ "ftp" = "${PROFILES_ACTIVE}" ]; then
     echo "Env [PROFILES_ACTIVE] is ${PROFILES_ACTIVE}, start ftpserver"
     exec java -jar /app.jar \
     --spring.profiles.active=${PROFILES_ACTIVE} \
-    --cloud.config.enabled=false \
+    --spring.cloud.config.enabled=false \
     --ftp.ip=${HOST_IP} \
     --ftp.port=${FTP_PORT} \
     --ftp.hostname=${HOST_NAME} \
@@ -18,7 +18,7 @@ elif [ "proxy" = "${PROFILES_ACTIVE}" ]; then
     echo "Env [PROFILES_ACTIVE] is ${PROFILES_ACTIVE}, start ftp proxy"
     java -jar /app.jar \
     --spring.profiles.active=${PROFILES_ACTIVE} \
-    --cloud.config.enabled=false \
+    --spring.cloud.config.enabled=false \
     --zk.host=${ZOOKEEPER_HOST:-NULL} \
     --ftp.ip=${HOST_IP} \
     --ftp.hostname=${HOST_NAME} \
@@ -28,10 +28,10 @@ elif [ "local" = "${PROFILES_ACTIVE}" ]; then
     echo "Env [PROFILES_ACTIVE] is ${PROFILES_ACTIVE}, start local ftpserver"
     exec java -jar /app.jar \
     --spring.profiles.active=${PROFILES_ACTIVE} \
-    --cloud.config.enabled=false \
+    --spring.cloud.config.enabled=false \
     --zk.host=${ZOOKEEPER_HOST:-NULL} \
     --ftp.ip=${HOST_IP} \
-    --ftp.hostname=${HOST_HOSTNAME} \
+    --ftp.hostname=${HOST_NAME} \
     --kafka.host=${KAFKA_HOST:-NULL} \
     --seemmo.url=${SEEMMO_URL:-NULL} \
     --eureka.ip=${EUREKA_IP:-NULL} \
